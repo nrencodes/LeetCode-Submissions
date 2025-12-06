2de4f59082ef4678be033f9648c1eeb72fa0660b
nrencodes <141109782+nrencodes@users.noreply.github.com>
2025-12-06 10:22:50 -0600

    Time: 19 ms (71.38%) | Memory: 18.5 MB (40.40%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -4,30 +4,23 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         maxArea = 0
 
-        def bfs(r, c):
-            queue = deque()
+        def dfs(r, c):
+            if(r < 0 or c < 0 or 
+                r >= len(grid) or c >= len(grid[0])
+                or grid[r][c] == 0):
+                return 0
+            
             grid[r][c] = 0
-            queue.append((r, c))
             area = 1
 
-            while queue:
-                r, c = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if(nr < 0 or nc < 0 or 
-                       nr >= len(grid) or nc >= len(grid[0])
-                       or grid[nr][nc] == 0):
-                       continue
-                    queue.append((nr, nc))   
-                    grid[nr][nc] = 0
-                    area += 1
-                    print(area)
+            for dr, dc in directions:
+                area += dfs(r+dr, c+dc)
+               
             return area
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-
-                    maxArea = max(maxArea, bfs(r, c))
+                    maxArea = max(maxArea, dfs(r, c))
 
         return maxArea
