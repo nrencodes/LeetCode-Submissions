b3b1509cc685f3e69176d27b5388cde821862823
nrencodes <141109782+nrencodes@users.noreply.github.com>
2025-12-23 15:38:17 -0600

    Time: 26 ms (94.29%), Space: 19.5 MB (13.66%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -4,20 +4,23 @@ class Solution:
             return False
         
         h = Counter(hand)
+        keys = sorted(h.keys()) 
 
-        minH = list(h.keys())
-        heapq.heapify(minH)
-        while minH:
-            first = minH[0]
-            for i in range(first, first + groupSize):
-                if i not in h:
+        for x in keys:
+            # How many lists are started from the current key
+            need = h[x]
+            if need == 0:
+                continue
+
+            # iterate over the current number and two consecutive numbers in the frequency chart
+            for v in range(x, x + groupSize):
+                # if the next numbers don't have as many occurrences needed to create the "need" number
+                # of sets, it means there will be an incomplete set so immediately return False. 
+                if h[v] < need:
                     return False
-                h[i] -= 1 
-                if h[i] == 0:
-                    if i != minH[0]:
-                        return False
-                    heapq.heappop(minH)
-        
+                # Otherwise, decrement the needed amount of the number and progress to the next key
+                h[v] -= need
+
         return True
             
             
